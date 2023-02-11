246bc362be1401d1e7e690140820abd807a0a9d9
EySoul <ijasolzzz@gmail.com>
2023-02-11 14:51:07 +0300

    login-page

--- frontend/src/pages/AuthPage/Login/Login.tsx
+++ frontend/src/pages/AuthPage/Login/Login.tsx
@@ -1,3 +1,38 @@
+import Input from "../../../components/ui/Input";
+import Button from "../../../components/ui/Button";
+import { useLoginForm } from "./useLoginForm";
+import Link from "../../../components/ui/Link";
+
+
 export const Login = () => {
-  return <div>login</div>;
+  const { register, onSubmit, errors,isLoading, alreadyExist} = useLoginForm();
+  return (
+    <>
+      <div className={'text-2xl mb-2'}>Авторизация</div>
+      <form onSubmit={onSubmit} className={'flex flex-col gap-2 w-1/2'}>
+        <Input
+          fullWidth
+          label="Логин"
+          type="text"
+          error = {errors.login?.message || alreadyExist.login}
+          required
+          {...register('login')}
+        />
+        <Input
+          fullWidth
+          label={"Пароль"}
+          type="password"
+          error = {errors.password?.message}
+          required
+          {...register('password')}
+        />
+        <Button type="submit" className={""} disabled = {isLoading}>
+          Войти
+        </Button>
+      </form>
+      <div className="text-md mt-2">
+        Нет аккаунта? <Link to ={'/auth/register'}>Зарегистрироваться</Link>
+      </div>
+    </>
+  );
 };

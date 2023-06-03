834abac15053c7b4bbadcf36a4a598af4dd6b947
Egor Frolov <egorfrolov2019.ef@gmail.com>
2023-06-03 04:17:18 +0300

    fixs

--- backend/scripts/golangci-lint-check.bash
+++ backend/scripts/golangci-lint-check.bash
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ "$(golangci-lint --version 2>/dev/null)" != "" ]; then
-  echo 'golangci-lint is found!';
-else\
-  echo 'start golangci-lint install';
+  exit 0;
+else
+  printf "golangci-lint not found. install...\n"
   curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b $(go env GOPATH)/bin latest;
+  exit 0;
 fi

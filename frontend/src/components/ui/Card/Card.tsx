11f2f2412e30f4c25b4e0dbad8eca3fba3c77781
Egor Frolov <egorfrolov2019.ef@gmail.com>
2023-06-03 03:18:31 +0300

    fix: pre-commit

--- frontend/src/components/ui/Card/Card.tsx
+++ frontend/src/components/ui/Card/Card.tsx
@@ -5,7 +5,6 @@ export type CardProps = HTMLAttributes<HTMLDivElement>;
 
 export const Card = forwardRef<HTMLDivElement, CardProps>(function Card(props, ref) {
     return (
-        <div
-          {...props} ref={ref} className={classNames(props.className, 'rounded border border-gray-150 bg-white')} />
+        <div {...props} ref={ref} className={classNames(props.className, 'rounded border border-gray-150 bg-white')} />
     );
 });

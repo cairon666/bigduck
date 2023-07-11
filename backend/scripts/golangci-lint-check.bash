382f3818163648565496c82f78d16148d01e3766
Egor Frolov <egorfrolov2019.ef@gmail.com>
2023-07-12 00:51:14 +0300

    add: dependency install into script file

--- backend/scripts/golangci-lint-check.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ "$(golangci-lint --version 2>/dev/null)" != "" ]; then
-  exit 0;
-else
-  printf "golangci-lint not found. install...\n"
-  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sudo sh -s -- -b $(go env GOPATH)/bin latest;
-  exit 0;
-fi
